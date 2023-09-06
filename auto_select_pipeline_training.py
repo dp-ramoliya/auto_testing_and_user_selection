6c193b00e502d9dc81ade13bee55c09ffabac869
Darshan <darshan@thinkbiz.co.in>
2023-09-06 15:13:13 +0530

    add code of auto select pipeline and auto select feature.

--- auto_select_pipeline_training.py
+++ auto_select_pipeline_training.py
@@ -1,28 +1,25 @@
 import os
 import joblib
-import warnings
 import psycopg2
+import warnings
 import numpy as np
 import pandas as pd
 from math import sqrt
 from termcolor import colored
-from sklearn.svm import LinearSVR
+from sklearn.linear_model import Lasso
+from datetime import timedelta, datetime
 from sklearn.pipeline import make_pipeline
-from tpot.builtins import StackingEstimator
 from sklearn.metrics import mean_squared_error
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.feature_selection import SelectFromModel
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.feature_selection import SelectPercentile, f_regression
-from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor, GradientBoostingRegressor
-
-from utills import db_to_pandas, input_df_wear, calc_rate
-from feature_select import feature_select_GB, feature_select_regression
-
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectFromModel, RFE
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
 warnings.filterwarnings("ignore")
 np.set_printoptions(precision=3, suppress=True)
+
+from utills import db_to_pandas, input_df_wear, calc_rate
 
 def df_wear_cleaning(df_wear):
     """
@@ -43,19 +40,29 @@ def all_asset_pid(asset_ids, conn):
     df_asset_pids = pd.read_sql_query(sql_query_pid, conn)
     return df_asset_pids
 
+create_dir = ['rmse', 'features', 'dynamic_train']
+for d in create_dir:
+    if not os.path.exists(f'./{d}'):
+        os.mkdir(f'./{d}')
+
 conn = psycopg2.connect(
    database="regression", user='postgres', password='root', host='127.0.0.1', port= '5432'
 )
 
+# ON STG
+# 6464/1117
+# 8980/1123
+# 11609/1111
+
 # data unit value come from step function
 # now use static for testing
-
-data_unit = 5
+data_unit = 4
 cursor = conn.cursor()
 sql_all_asset = 'SELECT * FROM t_measurement_item_id_relation WHERE unit={}'.format(data_unit)
 asset_measurement_id_relation_df = pd.read_sql_query(sql_all_asset, conn)
 
 all_asset_id = tuple(asset_measurement_id_relation_df.asset_id.unique())
+
 all_input=[]
 for i in all_asset_id:
     try:
@@ -63,7 +70,7 @@ for i in all_asset_id:
         all_input.append(out_wear)
     except Exception as e:
         print(e)
-        print("Error in input_df_wear function for asset_id: {}".format(i))  
+        print(f"Error in input_df_wear function for asset_id: {i}")
 
 df_wear = pd.concat(all_input, ignore_index=True)
 
@@ -79,11 +86,11 @@ with pd.option_context("mode.use_inf_as_na", True):
     ] = np.nan
     df_wear["finite_rate_mill_h"] = df_wear["rate_mill_h"].notna().astype(float)
     df_wear["rate_mill_h"].fillna(-9999, inplace=True)
-    # df_wear["rate_mill_h"].fillna(0, inplace=True)
+    #df_wear["rate_mill_h"].fillna(0, inplace=True)
     df_wear["finite_rate_mill_h"].value_counts()
-    
+
+
 df_asset_pid = all_asset_pid(all_asset_id, conn)
-pids_and_name = {i:j for i,j in zip(df_asset_pid['t_pid_no_text'].values, df_asset_pid['sensor_name'].values)}
 df_asset_pid.set_index('sensor_name', inplace=True)
 
 pids = tuple(df_asset_pid['t_pid_no_text'].values)
@@ -110,158 +117,131 @@ df_ai.loc[:, col_g] = df_ai.loc[:, col_g].mask(
 df_ai.mask(df_ai > 1e4, inplace=True)
 df_ai = df_ai.resample('D').mean()
 
-mld_pipeline_4 = make_pipeline(
-        SelectFromModel(estimator=ExtraTreesRegressor(max_features=0.7000000000000001, n_estimators=100), threshold=0.1),
-        KNeighborsRegressor(n_neighbors=43, p=2, weights="uniform")
-    )
-mld_pipeline_5 = make_pipeline(
-    SelectFromModel(estimator=ExtraTreesRegressor(max_features=0.8, n_estimators=100), threshold=0.1),
-    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
-    StackingEstimator(estimator=DecisionTreeRegressor(max_depth=9, min_samples_leaf=1, min_samples_split=8)),
-    RandomForestRegressor(bootstrap=True, max_features=1.0, min_samples_leaf=18, min_samples_split=12, n_estimators=100)
-)
-mld_pipeline_6 = make_pipeline(
-    SelectFromModel(estimator=ExtraTreesRegressor(max_features=0.55, n_estimators=100), threshold=0.3),
-    KNeighborsRegressor(n_neighbors=42, p=1, weights="uniform")
-)
-mld_pipeline_7 = make_pipeline(
-    SelectPercentile(score_func=f_regression, percentile=1),
-    StackingEstimator(estimator=DecisionTreeRegressor(max_depth=1, min_samples_leaf=4, min_samples_split=5)),
-    ExtraTreesRegressor(bootstrap=True, max_features=0.9500000000000001, min_samples_leaf=11, min_samples_split=4, n_estimators=100)
-)
-mld_pipeline_8 = make_pipeline(
-    StackingEstimator(estimator=LinearSVR(C=0.001, dual=True, epsilon=0.0001, loss="squared_epsilon_insensitive", tol=0.001)),
-    RandomForestRegressor(bootstrap=True, max_features=1.0, min_samples_leaf=7, min_samples_split=18, n_estimators=100)
-)
-
 model_prediction = []
 model_actual = []
 model_rmse = []
 
-item_id_relation = "SELECT * FROM t_measurement_item_id_relation"
-
-mdf = pd.read_sql_query(item_id_relation, conn)
-mdf = mdf[mdf['unit'] == data_unit]
-
-df_feature_GB = pd.DataFrame(columns=['asset_id', 'msumt_id', 'features'])
-
-master_feature = {}
 master_rmse = {}
-
-"""Generating Pickle file for All Asset ID"""
-break_outer_loop = False
-for m in all_asset_id:
-    print("Training of Mill/Asset Started ", m)
-    df_wear_asset = df_wear[df_wear["asset_id"]==m]
-    longitude = list(df_wear_asset['measurement_item_id'].unique())
-    model_list = [mld_pipeline_4,mld_pipeline_5,mld_pipeline_6,mld_pipeline_7,mld_pipeline_8]*3
-    model_pipe_dict = dict(map(lambda i,j : (i,j) , longitude,model_list))
-    all_sensor_id = df_asset_pid.loc[df_asset_pid['asset_id']== m]
-    dict_sensor_dfs = df_ai[all_sensor_id['t_pid_no_text']]
-
-    dict_sensor_dfs.dropna(inplace=True)
-    for r in df_wear_asset.measurement_item_id.unique():
-        print("logitude ", r)
-        dict_sensor_dfs = dict_sensor_dfs[:df_wear_asset.date[df_wear_asset['measurement_item_id']==r].max()]
-        loop_df_m = df_wear_asset[df_wear['measurement_item_id']==r]
-        sensor_df_list = []
-
-        for i in loop_df_m.index:
-            sensor_df_loop = dict_sensor_dfs[loop_df_m.loc[i,'date_start']:loop_df_m.loc[i, 'date']]
-            sensor_df_loop.dropna(inplace=True)
-            sensor_df_loop['{}_{}_rate_mill_h'.format(data_unit,r)]= loop_df_m.loc[i, 'rate_mill_h']
-            sensor_df_loop['{}_{}_wear_rate'.format(data_unit,r)] = loop_df_m.loc[i, 'wear_per_day']
-            sensor_df_list.append(sensor_df_loop)
-
-        sensor_data_with_wear = pd.concat(sensor_df_list)
-
-        inf_index = sensor_data_with_wear[sensor_data_with_wear['{}_{}_wear_rate'.format(data_unit,r)]==np.inf].index
-        sensor_data_with_wear.drop(inf_index, inplace=True)
-        sensor_data_with_wear.dropna(inplace=True)
-        sensor_data_with_wear.replace(to_replace = -9999, value = 0, inplace=True)
+master_feature = {}
+try:
+    for m in all_asset_id:
+        print("Training of Mill/Asset Started ", m)
+        df_wear_asset = df_wear[df_wear["asset_id"]==m]
+        longitude = list(df_wear_asset['measurement_item_id'].unique())
         
-        # Splitting the dataset into the Training set and Test set
-        X = sensor_data_with_wear.iloc[:,:7]
-        y = sensor_data_with_wear.iloc[:,-1:]
-
-        feature_selector = RandomForestRegressor(n_estimators=100)
-        feature_selector.fit(X, y)
-        important_features = X.columns[feature_selector.feature_importances_ > 0.05]
-
-        # L1 Regularization (Lasso) 
-        # feature_selector = SelectFromModel(Lasso(alpha=0.05)) 
-        # feature_selector.fit(X, y) 
-        # important_features = X.columns[feature_selector.get_support()]
-
-        print("important_features")
-        print(important_features)
-        master_feature[str(r)] = list(important_features)
-        feature_selected = df_asset_pid[df_asset_pid['t_pid_no_text'].isin(list(important_features))]
-        print("*"*50)
-        print(feature_selected)
-
-
-        models = [
-        ("randomforestregressor", RandomForestRegressor()),
-        ("gradientboostingregressor", GradientBoostingRegressor()),
-        ("extratreesregressor", ExtraTreesRegressor()),
-        # ("kneighborsregressor", KNeighborsRegressor()),
-        # ("decisiontreeregressor", DecisionTreeRegressor()),
-        ]
-
-        best_model = None
-        best_rmse = float("inf")
-
-        for model_name, model in models:
-            pipeline = make_pipeline(SelectFromModel(feature_selector), model)
-
-            # Hyperparameter optimization using GridSearchCV
-            # Hyperparameter optimization using GridSearchCV
-            param_grid = {
-                f"{model_name}__n_estimators": [100, 200, 300]
-            }
-
-            if model_name in [
-                "randomforestregressor",
-                "gradientboostingregressor",
-                "decisiontreeregressor",
-            ]:
-                param_grid[f"{model_name}__max_depth"] = [None, 5, 10]
-
-            grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring="neg_mean_squared_error")
-            grid_search.fit(X[important_features], y)
-
-            # Best model from grid search
-            current_model = grid_search.best_estimator_
-
-            # Model Evaluation
-            # Split the data into training and testing sets
-            X_train, X_test, y_train, y_test = train_test_split(X[important_features], y, test_size=0.2, random_state=42)
-
-            # Train the current model on the training set
-            current_model.fit(X_train, y_train)
-
-            # Use the current model to make predictions on the test set
-            y_pred = current_model.predict(X_test)
-
-            # Calculate the root mean squared error (RMSE)
-            current_rmse = sqrt(mean_squared_error(y_test, y_pred))
-
-            # Update the best model if the current model has a lower RMSE
-            if current_rmse < best_rmse:
-                best_model = current_model
-                best_rmse = current_rmse
-
-        master_rmse[str(r)] = best_rmse
-        model_rmse.append(best_rmse)
+        all_sensor_id = df_asset_pid.loc[df_asset_pid['asset_id']== m]
+        dict_sensor_dfs = df_ai[all_sensor_id['t_pid_no_text']]
         
-        master_feature_df = pd.DataFrame(master_feature.items())
-        master_feature_df.to_csv('features/{}_master_feature.csv'.format(m), index=False)
+        dict_sensor_dfs.dropna(inplace=True)
         
-        joblib.dump(current_model, 'training/model_{}.pkl'.format(r))
-        
-        print("Best Model:", colored(best_model, 'green', attrs=['bold'] ))
-        print("Best RMSE:",colored(best_rmse, 'red', attrs=['bold']))
-        print('rootMeanSqErr_of_{}_{} : {}'.format(data_unit,r,best_rmse))
-        print("#"*50)
-    
+        for r in df_wear_asset.measurement_item_id.unique():
+            dict_sensor_dfs = dict_sensor_dfs[:df_wear_asset.date[df_wear_asset['measurement_item_id']==r].max()]
+            print(colored(r, 'blue', attrs=['bold']), "--",df_wear_asset.date[df_wear_asset['measurement_item_id']==r].max())
+
+            loop_df_m = df_wear_asset[df_wear['measurement_item_id']==r]
+            sensor_df_list = []
+            for i in loop_df_m.index:
+                sensor_df_loop = dict_sensor_dfs[loop_df_m.loc[i,'date_start']:loop_df_m.loc[i, 'date']]
+                sensor_df_loop.dropna(inplace=True)
+                sensor_df_loop['{}_{}_rate_mill_h'.format(data_unit,r)]= loop_df_m.loc[i, 'rate_mill_h']
+                sensor_df_loop['{}_{}_wear_rate'.format(data_unit,r)] = loop_df_m.loc[i, 'wear_per_day']
+                sensor_df_list.append(sensor_df_loop)
+
+            sensor_data_with_wear = pd.concat(sensor_df_list)
+
+            inf_index = sensor_data_with_wear[sensor_data_with_wear['{}_{}_wear_rate'.format(data_unit,r)]==np.inf].index
+            sensor_data_with_wear.drop(inf_index, inplace=True)
+            sensor_data_with_wear.dropna(inplace=True)
+            sensor_data_with_wear.replace(to_replace = -9999, value = 0, inplace=True)
+
+            X = sensor_data_with_wear.iloc[:,:7]
+            y = sensor_data_with_wear.iloc[:,-1:]
+
+            feature_selector = RandomForestRegressor(n_estimators=100)
+            feature_selector.fit(X, y)
+            important_features = X.columns[feature_selector.feature_importances_ > 0.05]
+
+            # L1 Regularization (Lasso) 
+            # feature_selector = SelectFromModel(Lasso(alpha=0.05)) 
+            # feature_selector.fit(X, y) 
+            # important_features = X.columns[feature_selector.get_support()]
+
+            print("important_features")
+            print(important_features)
+            master_feature[str(r)] = list(important_features)
+            feature_selected = df_asset_pid[df_asset_pid['t_pid_no_text'].isin(list(important_features))]
+            print("*"*50)
+            print(feature_selected)
+
+
+            models = [
+            ("randomforestregressor", RandomForestRegressor()),
+            ("gradientboostingregressor", GradientBoostingRegressor()),
+            ("extratreesregressor", ExtraTreesRegressor()),
+            # ("kneighborsregressor", KNeighborsRegressor()),
+            # ("decisiontreeregressor", DecisionTreeRegressor()),
+            ]
+
+            best_model = None
+            best_rmse = float("inf")
+
+            for model_name, model in models:
+                pipeline = make_pipeline(SelectFromModel(feature_selector), model)
+
+                # Hyperparameter optimization using GridSearchCV
+                # Hyperparameter optimization using GridSearchCV
+                param_grid = {
+                    f"{model_name}__n_estimators": [100, 200, 300]
+                }
+
+                if model_name in [
+                    "randomforestregressor",
+                    "gradientboostingregressor",
+                    "decisiontreeregressor",
+                ]:
+                    param_grid[f"{model_name}__max_depth"] = [None, 5, 10]
+
+                grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring="neg_mean_squared_error")
+                grid_search.fit(X[important_features], y)
+
+                # Best model from grid search
+                current_model = grid_search.best_estimator_
+
+                # Model Evaluation
+                # Split the data into training and testing sets
+                X_train, X_test, y_train, y_test = train_test_split(X[important_features], y, test_size=0.2, random_state=42)
+
+                # Train the current model on the training set
+                current_model.fit(X_train, y_train)
+
+                # Use the current model to make predictions on the test set
+                y_pred = current_model.predict(X_test)
+
+                # Calculate the root mean squared error (RMSE)
+                current_rmse = sqrt(mean_squared_error(y_test, y_pred))
+
+                # Update the best model if the current model has a lower RMSE
+                if current_rmse < best_rmse:
+                    best_model = current_model
+                    best_rmse = current_rmse
+
+            master_rmse[str(r)] = best_rmse
+            model_rmse.append(best_rmse)
+            
+            joblib.dump(current_model, 'dynamic_train/model_{}.pkl'.format(r))
+            
+            print("Best Model:", colored(best_model, 'green', attrs=['bold'] ))
+            print("Best RMSE:",colored(best_rmse, 'red', attrs=['bold']))
+            print('rootMeanSqErr_of_{}_{} : {}'.format(data_unit,r,best_rmse))
+            print("#"*50)
+
+except KeyboardInterrupt:
+    pass
+
+master_feature_df = pd.DataFrame(master_feature.items())
+master_feature_df.to_csv(f'features/{data_unit}_master_feature.csv', index=False)
+
+master_rmse_df = pd.DataFrame(master_rmse.items(), columns=["measurement_item_id", "RMSE"])
+#master_rmse_df["RMSE"] = master_rmse_df["RMSE"].map(lambda x: "{:.20f}".format(x))
+master_rmse_df.to_csv('rmse/{}_master_rmse.csv'.format(data_unit), index=False)
+print(master_rmse_df)
